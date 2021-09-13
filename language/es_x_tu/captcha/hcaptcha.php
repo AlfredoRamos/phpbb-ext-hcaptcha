7ada800c5ac7747eeb92314f42bb35c643f7c589
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-09-12 19:10:41 -0500

    Update Spanish translations

--- language/es_x_tu/captcha/hcaptcha.php
+++ language/es_x_tu/captcha/hcaptcha.php
@@ -41,6 +41,8 @@ $lang = array_merge($lang, [
 	'HCAPTCHA_NOT_AVAILABLE' => 'Para poder utilizar hCaptcha, debes crear una cuenta en <a href="https://www.hcaptcha.com/" rel="external nofollow noreferrer noopener" target="_blank">www.hcaptcha.com</a>.',
 	'HCAPTCHA_INCORRECT' => 'La solución que proporcionaste es incorrecta.',
 	'HCAPTCHA_NOSCRIPT' => 'Por favor, habilita JavaScript en tu navegador web para cargar el desafío.',
+	'HCAPTCHA_LOGIN_ERROR_ATTEMPTS' => 'Has superado el número máximo de intentos de inicio de sesión permitidos.<br>Además de tu nombre de usuario y contraseña, se utilizará hCaptcha para autenticar tu sesión.',
+	'HCAPTCHA_REQUEST_EXCEPTION' => 'Error de solicitud de hCaptcha: %s',
 
 	'ACP_HCAPTCHA_TOGGLE_SECRET' => 'Alternar visibilidad de %s',
 	'ACP_HCAPTCHA_VALIDATE_INVALID_FIELDS' => 'Valores inválidos para los campos: <samp>%s</samp>'
