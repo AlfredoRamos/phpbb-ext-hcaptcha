5cabb395a3bfaee07f4d04d34a05be017d8620cf
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2021-10-21 21:06:31 -0500

    Fix validation issues

--- adm/style/js/settings.js
+++ adm/style/js/settings.js
@@ -8,27 +8,50 @@
 (function() {
 	'use strict';
 
+	// Element.closest() polifyll
+	// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#polyfill
+	if (!Element.prototype.closest) {
+		if (!Element.prototype.matches) {
+			Element.prototype.matches = (Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector);
+		}
+
+		Element.prototype.closest = function(s) {
+			let el = this;
+
+			do {
+				if (Element.prototype.matches.call(el, s)) {
+					return el;
+				}
+
+				el = (el.parentElement || el.parentNode);
+			} while (el !== null && el.nodeType === 1);
+
+			return null;
+		};
+	}
+
+	// Toggle hCaptcha secret key
 	document.body.addEventListener('click', function(e) {
-		let toggle = e.target.closest('#toggle-hcaptcha-secret');
+		const toggle = e.target.closest('#toggle-hcaptcha-secret');
 
 		if (!toggle) {
 			return;
 		}
 
-		let field = document.body.querySelector('#hcaptcha-secret');
-		let icon = toggle.querySelector('.icon');
+		const field = document.body.querySelector('#hcaptcha-secret');
+		const icon = toggle.querySelector('.icon');
 
 		if (!field || !icon) {
 			return;
 		}
 
-		let isHidden = (field.getAttribute('type').trim() === 'password');
+		const isHidden = (field.getAttribute('type').trim() === 'password');
 
 		// Toggle field type
 		field.setAttribute('type', (isHidden ? 'text' : 'password'));
 
 		// Toggle icon
-		icon.classList.toggle('fa-eye-slash', isHidden);
-		icon.classList.toggle('fa-eye', !isHidden);
+		icon.classList.toggle('fa-eye-slash');
+		icon.classList.toggle('fa-eye');
 	});
 })();
