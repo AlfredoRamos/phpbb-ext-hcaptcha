bdb6d96dce712fa495923d01fa63e12ab762017b
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2026-03-03 22:13:09 -0600

    New translations hcaptcha.php (Russian)

--- language/ru/captcha/hcaptcha.php
+++ language/ru/captcha/hcaptcha.php
@@ -25,7 +25,7 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, [
 	'CAPTCHA_HCAPTCHA' => 'hCaptcha',
-	'CAPTCHA_HCAPTCHA_EXPLAIN' => 'Посмотрите <a href="https://www.phpbb.com/customise/db/extension/hcaptcha/faq" rel="external nofollow noreferrer noopener" target="_blank"><strong>FAQ</strong></a> для более подробной информации. Если вам необходима помощь, пожалуйста, перейдите в раздел <a href="https://www.phpbb.com/customise/db/extension/hcaptcha/support" rel="external nofollow noreferrer noopener" target="_blank"><strong>Поддержка</strong></a>.',
+	'CAPTCHA_HCAPTCHA_EXPLAIN' => '<p>Consult the <a href="%1$s" rel="external nofollow noreferrer noopener" target="_blank"><strong>FAQ</strong></a> for more information. If you require assistance, please visit the <a href="%2$s" rel="external nofollow noreferrer noopener" target="_blank"><strong>Support</strong></a> section.</p><p>If you like or found this extension useful and want to show some appreciation, you can consider supporting its development by <a href="%3$s" rel="external nofollow noreferrer noopener" target="_blank"><strong>giving a donation</strong></a>.</p>',
 	'HCAPTCHA_KEY' => 'Ключ сайта',
 	'HCAPTCHA_KEY_EXPLAIN' => 'Ключ сайта сгенерирован на hCaptcha для вашего домена.',
 	'HCAPTCHA_SECRET' => 'Секретный ключ',
@@ -38,6 +38,8 @@ $lang = array_merge($lang, [
 	'HCAPTCHA_SIZE_EXPLAIN' => 'Размер виджета hCaptcha.',
 	'HCAPTCHA_SIZE_NORMAL' => 'Обычный',
 	'HCAPTCHA_SIZE_COMPACT' => 'Компактный',
+	'HCAPTCHA_FORCE_LOGIN' => 'Force spambot countermeasures in logins',
+	'HCAPTCHA_FORCE_LOGIN_EXPLAIN' => 'Requires users to always pass the anti-spambot task to help prevent automated logins.',
 	'HCAPTCHA_NOT_AVAILABLE' => 'Чтобы использовать hCaptcha, необходимо создать учетную запись на сайте <a href="https://www.hcaptcha.com/" rel="external nofollow noreferrer noopener" target="_blank">www.hcaptcha.com</a>.',
 	'HCAPTCHA_INCORRECT' => 'Вы указали неправильное решение.',
 	'HCAPTCHA_NOSCRIPT' => 'Для загрузки включите JavaScript в вашем браузере.',
